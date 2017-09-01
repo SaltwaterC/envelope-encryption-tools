b9042311721ca55a7ab224627a6bf376548a35c2
SaltwaterC <saltwaterc@gmail.com>
2017-09-01 17:52:09 +0100

    Test PKCS1 and OAEP padding against crypto.

--- test/rsa.test.js
+++ test/rsa.test.js
@@ -2,6 +2,7 @@
 
 /*global describe: true, before: true, it: true*/
 
+var fs = require('fs');
 var crypto = require('crypto');
 
 var rsa = require('../lib/main').rsa;
@@ -12,18 +13,32 @@ describe('RSA tests', function() {
   var payload1, payload2, payload3, message1, message2, message3;
 
   before(function(done) {
+    var expect = 3;
+    var actual = 0;
+
+    var finish = function() {
+      actual++;
+      if (actual === expect) {
+        done();
+      }
+    };
+
     crypto.randomBytes(32, function(err, bytes) {
       assert.ifError(err);
       payload1 = bytes;
-      crypto.randomBytes(32, function(err, bytes) {
-        assert.ifError(err);
-        payload2 = bytes;
-        crypto.randomBytes(32, function(err, bytes) {
-          assert.ifError(err);
-          payload3 = bytes;
-          done();
-        });
-      });
+      finish();
+    });
+
+    crypto.randomBytes(32, function(err, bytes) {
+      assert.ifError(err);
+      payload2 = bytes;
+      finish();
+    });
+
+    crypto.randomBytes(32, function(err, bytes) {
+      assert.ifError(err);
+      payload3 = bytes;
+      finish();
     });
   });
 
@@ -73,9 +88,39 @@ describe('RSA tests', function() {
       });
     });
 
+    it('should decrypt test string with default OAEP padding using crypto module', function(done) {
+      fs.readFile('test/data/private.pem', function(err, key) {
+        assert.ifError(err, 'we have an error');
+
+        var decrypted = crypto.privateDecrypt({
+          key: key,
+          padding: crypto.constants.RSA_PKCS1_OAEP_PADDING
+        }, new Buffer(message1, 'base64'));
+
+        assert.strictEqual(decrypted.toString('binary'), payload1.toString('binary'), 'the test string is sucessfully decrypted');
+
+        done();
+      });
+    });
+
     it('should decrypt test string with PKCS1_V1_5 padding', function(done) {
       rsa.decrypt('test/data/private.pem', rsa.padding.PKCS1_V1_5, message2, function(err, decrypted) {
         assert.ifError(err, 'we have an error');
+
+        assert.strictEqual(decrypted.toString('binary'), payload2.toString('binary'), 'the test string is sucessfully decrypted');
+
+        done();
+      });
+    });
+
+    it('should decrypt test string with PKCS1_V1_5 padding using crypto module', function(done) {
+      fs.readFile('test/data/private.pem', function(err, key) {
+        assert.ifError(err, 'we have an error');
+
+        var decrypted = crypto.privateDecrypt({
+          key: key,
+          padding: crypto.constants.RSA_PKCS1_PADDING
+        }, new Buffer(message2, 'base64'));
 
         assert.strictEqual(decrypted.toString('binary'), payload2.toString('binary'), 'the test string is sucessfully decrypted');
 
